ee2803ba6804e66e514c9df81b82fb41c3fbf51e
ivan <ivanchin@MacBook-Pro-28>
2022-06-03 01:05:06 +0800

    sign-up & login authentication with firebase flow and password reset flow

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRw8HAEMuCYtxtZnAnmqxfHzu5MTmsFf4",
+  authDomain: "connectinus-9b1c6.firebaseapp.com",
+  projectId: "connectinus-9b1c6",
+  storageBucket: "connectinus-9b1c6.appspot.com",
+  messagingSenderId: "1021105229648",
+  appId: "1:1021105229648:web:fd240ff7ca3db9f737e655",
+  measurementId: "G-XLRTCGK3HD"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getFirestore(app);

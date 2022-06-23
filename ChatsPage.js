ea09ced42bdeadd86fae3de9adef423e00193a86
ivan <ivanchin@MacBook-Pro-28>
2022-06-23 11:05:43 +0800

    Debugged Chat

--- ChatsPage.js
+++ ChatsPage.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react'
+
 import {
   StyleSheet,
   View,
   Text,
   Pressable,
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getAuth } from "firebase/auth";
+import { doc, onSnapshot, query, where, setDoc, Timestamp, getFirestore, collection, getDoc } from 'firebase/firestore';
+import { useIsFocused } from "@react-navigation/native";
+
+
 
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export default function ChatsPage({ navigation }) {
+    let auth = getAuth();
+    const db = getFirestore()
+    const [user, setUser] = useState('')
+    const [userData, setUserData] = useState(null);
+    const isFocused = useIsFocused();
+    
+    const getUser = async() => {
+        const docRef = doc(collection(db, "users"), auth.currentUser.uid);
+        const docSnap = await getDoc(docRef);
+        if( docSnap.exists() ) {
+            console.log('User Data:', docSnap.data());
+            setUserData(docSnap.data());
+        } else {
+            console.log('No such User Document')
+        }
+      }
+      useEffect(() => {
+          if(isFocused) {
+            getUser();
+          }
+      }, [navigation, isFocused]);
 
-    const onPressHandler = () => {
-        navigation.navigate('InnerChatsPage');
+
+
+    async function onPressHandler() {
+      const _id = auth.currentUser.email
+      const name = userData ? userData.displayName : 'null' 
+      const user = { _id, name }
+      await AsyncStorage.setItem('user', JSON.stringify(user))
+      setUser(user)
+      navigation.navigate('InnerChatsPage', {user});
+
     }
 
     return (
